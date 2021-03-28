0550e50e1fafb9dfdbab81f6042bc4f1c2ee51af
Stephen Aziz <stephenaziz.sa@gmail.com>
2021-03-28 15:51:50 +0100

    plan index.js and write Book class

---
+++ index.js
@@ -0,0 +1,13 @@
+// Book Class: represets a book
+class Book {
+    constructor(author, title) {
+        this.author = author;
+        this.title = title;
+        return this;
+    }
+}
+// UI class: UI functions and helpers
+
+// Data Storage class: handles storing and getting books
+
+// Events
